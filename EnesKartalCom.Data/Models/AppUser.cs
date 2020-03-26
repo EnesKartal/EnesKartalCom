7de4e9b52679a56fe6a7c57548365a0d0a093f50
e.kartal@szutech.com <e.kartal@szutech.com>
2020-03-26 23:46:36 +0300

    db olusturulacak

---
+++ EnesKartalCom.Data/Models/AppUser.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EnesKartalCom.Data.Models
+{
+    public class AppUser : BaseEntity
+    {
+        public AppUser()
+        {
+            AppUserLogs = new HashSet<AppUserLog>();
+        }
+
+        public string Email { get; set; }
+        public string Token { get; set; }
+        public bool IsActive { get; set; }
+        public string Mobile { get; set; }
+        public string Lastname { get; set; }
+        public string Password { get; set; }
+        public string TcNumber { get; set; }
+        public string Firstname { get; set; }
+        public string LastLoginIp { get; set; }
+        public DateTime LastLoginDate { get; set; }
+
+
+        [ForeignKey("Id")]
+        public virtual IEnumerable<AppUserLog> AppUserLogs { get; set; }
+    }
+
+    public class AppUserLog : BaseEntity
+    {
+        public string Type { get; set; }
+        public DateTime Date { get; set; }
+        public int AppUserId { get; set; }
+        public string IpAddress { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ EnesKartalCom/Models/Account/LoginInput.cs
@@ -0,0 +1,9 @@
+﻿namespace EnesKartalCom.Models
+{
+    public class LoginInput
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string CaptchaKey { get; set; }
+    }
+}
